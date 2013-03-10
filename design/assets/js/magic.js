3a6ecd303ba1d9212466ae1120c7aa8e68872b9b
King Sidharth <king@kingsidharth.com>
2013-03-10 23:11:35 +0530

    Make it responsive

--- design/assets/js/magic.js
+++ design/assets/js/magic.js
@@ -2,10 +2,15 @@ $(function() {
   /* -- Universal -- */
 
   // Set minimum height of the main frame to window to avoid awkward footer in mid air
-  var winHeight = $(window).height();
-  $('#content').css('min-height', winHeight);
-  $('#sidebar').css('min-height', winHeight);
+  var winWidth = $(window).width();
+  var winHeight = $(window).height();  
+  if (winWidth > 1000) {
+    $('#content').css('min-height', winHeight);
+    $('#sidebar').css('min-height', winHeight);
   
-  var sidebarWidth = $('#sidebar').width();
-  $('#fixed-side').css('width', sidebarWidth);
+    var sidebarWidth = $('#sidebar').width();
+    $('#fixed-side').css('width', sidebarWidth);
+  } else {
+    $('#content').css('margin-bottom', winHeight);
+  }
 })(jQuery)
