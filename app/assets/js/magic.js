406cf2e5cfc9ba4fcb7e26fc2842dd26a92ef8d2
King Sidharth <king.sidharth@yahoo.com>
2014-01-05 22:52:38 +0530

    Make it work with yeoman

--- app/assets/js/magic.js
+++ app/assets/js/magic.js
@@ -3,8 +3,8 @@ $(function() {
 
   // Set minimum height of the main frame to window to avoid awkward footer in mid air
   var winWidth = $(window).width();
-  var winHeight = $(window).height();  
-  if (winWidth > 1000) {
+  var winHeight = $(window).height();
+  /*if (winWidth > 1000) {
     $('#content').css('min-height', winHeight);
     $('#sidebar').css('min-height', winHeight);
   
@@ -12,5 +12,5 @@ $(function() {
     $('#fixed-side').css('width', sidebarWidth);
   } else {
     $('#content').css('margin-bottom', winHeight);
-  }
-})(jQuery)
+  }*/
+})(jQuery);
