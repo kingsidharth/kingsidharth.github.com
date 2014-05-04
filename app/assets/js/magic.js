d9c6bbe44f79ac75171cb757803b0798ef32f357
King Sidharth <king.sidharth@yahoo.com>
2014-05-04 22:54:16 +0530

    Add lib.js

--- app/assets/js/magic.js
+++ app/assets/js/magic.js
@@ -1,100 +1,4 @@
 
-var debug = true;
-var height, width, device;
-var scrollPosition;
-
-
-// Debugging Message Control
-var debug_stage_msg = function(message) {
-  if(debug) {
-    console.log(message);
-  }
-}
-
-var dsm = debug_stage_msg; // Alias
-
-
-// Event Tracking Wrapper
-var shoutOut = function(category, action, opt_label, opt_value) {
-
-  opt_label = opt_label || false;
-  opt_value = opt_value || false;
-
-  // Google Analytics Shit
-  _gaq.push(['_trackEvent', category, action, opt_label, opt_value]);
-
-  dsm("An event was logged");
-
-}
-
-// CSS Methods
-var setCSS = function(target, property, value) {
-  $(target).css(property, value);
-}
-
-var setWidth = function(target, value) {
-  setCSS(target, 'min-width', Math.round(value));
-}
-
-var setHeight = function(target, value) {
-  setCSS(target, 'min-height', Math.round(value));
-}
-
-var percentage = function(number, percentage, decimalPlaces) {
-  decimalPlaces = decimalPlaces || false; 
-
-  var result = (percentage/100) * number; 
-
-  if (decimalPlaces) {
-    return Math.floor(result * (10 * decimalPlaces)) / (10 * decimalPlaces);
-  } else {
-    return result;
-  }
-}
-
-// Get Device Type — Responsive Querries
-var getDeviceType = function(width, height) {
-  if (width > 1600 && height > 1100) {
-    device = 'desk-wide';
-  }
-  else if (width > 1000) {
-    device = 'desk';
-  }
-  else if (width < 1000 && width > 481) {
-    device = 'portable';
-  }
-  else if (width < 481) {
-    device = 'palm';
-  }
-  else {
-    device = 'unknown';
-  }
-
-  return device;
-}
-
-var deviceDimensions = function() {
-  // Get to know the viewport
-  // https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
-
-  window.width = Math.max(window.innerWidth)
-  window.height = Math.max(window.innerHeight)
-
-  return width, height;
-}
-
-// Classes that need viewport data to Initialize
-var viewportDependentClasses = function(device) {
-  if(device != 'desk-wide') {
-    $('.full-height').css('min-height', height);
-    $('.half-height').css('min-height', height / 2);
-  } else {
-    setHeight('.full-height', 900);
-    setHeight('.half-height', 1500);
-  }
-
-  return false; }
-
 /* ==== HEADER STATICNESS ==== */
 var stickyOffset, staticHeaderHeight;
   
@@ -114,7 +18,6 @@ var layout = function() {
   deviceDimensions();
   getDeviceType(width, height);
   viewportDependentClasses(device);
-
 
   if(device === 'desk-wide' || device === 'desk') {
     stickyHeader('#header_area');
